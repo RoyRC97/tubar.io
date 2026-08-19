e851fa2c8ca3f41e964c71dd511f3a31169c6872
Rodrigo Contreras <97rodrigorioscontreras@gmail.com>
2026-08-19 15:53:05 -0600

    Update Firebase initialization and configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA8Sl7nDm6Aw_WJBDCwXfLfk29n7tLqako",
   authDomain: "tubar-app.firebaseapp.com",
@@ -13,5 +7,5 @@ const firebaseConfig = {
   appId: "1:146267640357:web:e9cccb97a63f076d0065cc"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
